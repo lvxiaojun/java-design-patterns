ba10aa86da382d5bb79cb90e61ebcbc9139b8b44
lvxiaojun <760285803@qq.com>
2019-07-24 17:56:47 +0800

    update

---
+++ src/main/java/com/creational/factory/simple/AppleMobile.java
@@ -0,0 +1,9 @@
+package com.creational.factory.simple;
+
+public class AppleMobile extends Mobile{
+
+	@Override
+	public void sayHello() {
+		System.out.println("hello,欢迎使用苹果手机！");
+	}
+}
---
+++ src/main/java/com/creational/factory/simple/HuaweiMobile.java
@@ -0,0 +1,10 @@
+package com.creational.factory.simple;
+
+public class HuaweiMobile extends Mobile{
+
+	@Override
+	public void sayHello() {
+		System.out.println("hello,欢迎使用华为手机！");
+	}
+	
+}
